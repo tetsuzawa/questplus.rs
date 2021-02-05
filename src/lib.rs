619f8e4818d176e548145cac24f1fbb387b1e39b
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-02-05 23:38:24 +0900

    Add types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+use std::collections::HashMap;
+use ndarray::prelude::*;
+use num::{Float, Num};
+
+struct QuestPlus<F, O>
+    where
+        F: Fn(/*TODO args*/),
+{
+    stim_domain: HashMap<String, Array1<f64>>,
+    param_domain: HashMap<String, Array1<f64>>,
+    outcome_domain: HashMap<String, O>,
+    prior: Option<HashMap<String, Array1<f64>>>,
+    func: F,
+    stim_scalse: StimScale,
+    stim_selection_method: StimSelectionMethod,
+    // stim_selection_options: TODO
+    param_estimation_method: ParamEstimationMethod,
+}
+
+enum StimScale {
+    Linear,
+    Log10,
+    Decibel,
+}
+
+enum StimSelectionMethod {
+    MinEntropy,
+    MinNEntropy(i32),
+}
+
+enum ParamEstimationMethod {
+    Mode,
+    Mean,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
